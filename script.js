346eae8aef5a080b66f3c32c8673f3abbeccd579
Mary Kathryn <msmarykathrynholt@gmail.com>
2020-10-10 16:07:01 -0500

    added files for background

--- script.js
+++ script.js
@@ -2,5 +2,3 @@
 
 $( document ).ready()
 
-<a href="#" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Primary link</a>
-<a href="#" class="btn btn-secondary btn-lg active" role="button" aria-pressed="true">Link</a>
